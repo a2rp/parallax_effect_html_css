2e78e406d01618aa601e41a643a6871dc4b5d206
ash1198 <ash.ranjan09@gmail.com>
2023-08-11 02:12:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+    // window.onscroll = (event) => {
+    //     let top = window.scrollY * 0.05;
+    //     top = top < 1 ? 1 : window.scrollY;
+    //     document.querySelector(".background-image").style.cssText = `
+    //         transform: scale(${top});
+    //     `;
+    // };
+};
+
